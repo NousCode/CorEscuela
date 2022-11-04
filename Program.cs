9c10fd899eee9ae698c2c41cf4e08367c29b12ad
Juan Camilo Salazar <juancspace@outlook.com>
2022-11-04 15:06:08 -0500

    Creating a new class, its name is Curso.

--- Program.cs
+++ Program.cs
@@ -2,5 +2,20 @@
 using CoreEscuela.Entidades;
 
 var escuela = new Escuela("Soul Code Academy", 2022, TiposEscuela.Primaria, pais: "Colombia", ciudad: "Pereira");
+var curso1 = new Curso() {
+    Nombre = "101"    
+};
+
+var curso2 = new Curso() {
+    Nombre = "201"    
+};
+
+var curso3 = new Curso() {
+    Nombre = "301"    
+};
 
 Console.WriteLine(escuela);
+Console.WriteLine("================");
+Console.WriteLine(curso1.Nombre + "," + curso1.UniqueId);
+Console.WriteLine($"{curso2.Nombre} , {curso2.UniqueId}");
+Console.WriteLine($"{curso3.Nombre} , {curso3.UniqueId}");
