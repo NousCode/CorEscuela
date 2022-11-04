330479446b1336619176b29353d6b010b259d3ad
Juan Camilo Salazar <juancspace@outlook.com>
2022-11-03 23:24:43 -0500

    Hello World dotnetgit add .

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// * En la parte más top del programa sera la función main de .net
+var miEscuela = new Escuela();
+miEscuela.nombre = "Soul Code Academy";
+miEscuela.direccion = "Mz 31 Cs 09 Br Jardín";
+miEscuela.añofundacion = 2010;
+miEscuela.director = "Liliana Restrepo";
+Console.WriteLine("Timbrar!!");
+miEscuela.Timbrar();
+
+class Escuela
+{
+    // Atributos
+    public string nombre;
+    public string direccion;
+    public int añofundacion;
+    public string director;
+
+    // Métodos
+    public void Timbrar()
+    {
+        Console.Beep();
+    }
+}
